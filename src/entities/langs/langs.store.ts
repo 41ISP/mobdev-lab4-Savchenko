1584a16caf63aefc97d5953cfd316492b8db1967
hzrxrdx <131643508+hzrxrdx@users.noreply.github.com>
2025-02-01 11:23:26 +0000

    add last langs store

--- src/entities/langs/langs.store.ts
+++ src/entities/langs/langs.store.ts
@@ -2,21 +2,37 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface ITranslationStore {
-    lang: string[]
-    setLang: (langs: string[]) => void
+  lang: string[];
+  lastLangs: string[];
+  history: string[];
+  addHistory: (lastWord: string) => void;
+  setLang: (langs: string[]) => void;
+  setLastLangs: (langs: string[]) => void;
+  clearHistory: () => void;
 }
 
 const useTranslationStore = create<ITranslationStore>()(
-    persist(
-
-        (set) => ({
-        lang: [],
-        setLang: (langs) => set((state) => ({...state, lang: langs})),
+  persist(
+    (set) => ({
+      lang: [],
+      lastLangs: ["en", "ru"],
+      history: [],
+      setLang: (langs) => set((state) => ({ ...state, lang: langs })),
+      setLastLangs: (langs) => set((state) => ({ ...state, lastLangs: langs })),
+      addHistory: (lastWord) =>
+        set((state) => {
+          if (state.history.length < 4) {
+            return { ...state, history: [...state.history, lastWord] };
+          } else {
+            return { ...state, history: [...state.history.slice(1), lastWord] };
+          }
+        }),
+      clearHistory: () => set((state) => ({ ...state, history: [] })),
     }),
     {
-        name: 'Langs',
+      name: "Langs",
     }
-    )
-)
+  )
+);
 
-export default useTranslationStore
+export default useTranslationStore;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,22 +2,22 @@ import { useState } from "react";
 import useTranslationStore from "../entities/langs/langs.store";
 import DictionaryAPI, { IApiResponse } from "../shared/api/DictionaryAPI";
 import { ITranslate } from "../entities/translate/translate.model";
-import "./Search.css"
+import "./Search.css";
 
 const Search = () => {
+  const { lang, lastLangs, setLastLangs, history, addHistory, clearHistory} =
+    useTranslationStore((state) => state);
   const [word, setWord] = useState<string>("");
-  const [firstLang, setFirstLang] = useState<string>("");
-  const [secondLang, setSecondLang] = useState<string>("");
-  const { lang } = useTranslationStore((state) => state);
+  const [firstLang, setFirstLang] = useState<string>(lastLangs[0]);
+  const [secondLang, setSecondLang] = useState<string>(lastLangs[1]);
   const [a, setA] = useState<IApiResponse<ITranslate> | undefined>(undefined);
-
   const handleTranslate = async () => {
+    addHistory(word);
     const res = await DictionaryAPI.translateWord({
       word,
       firstLang,
       secondLang,
     });
-    res.data && res.data;
     setA(res);
   };
   return (
@@ -25,7 +25,7 @@ const Search = () => {
       <div className="searchDiv">
         <h1>DictionaryAPI</h1>
         <input
-        className="search"
+          className="search"
           type="text"
           value={word}
           placeholder="Введите слово"
@@ -33,12 +33,19 @@ const Search = () => {
             setWord(e.currentTarget.value);
           }}
         />
+        <div className="historyDiv">
+          {history.slice(0, -1).reverse().map((el) => (
+            <span>{el}</span>
+          ))}
+          <span className="clearCross" onClick={clearHistory}>✕</span>
+        </div>
         <select
           className="lang1"
           name="Language1"
           value={firstLang}
           onChange={(e) => {
             setFirstLang(e.currentTarget.value);
+            setLastLangs([e.currentTarget.value, lastLangs[1]]);
           }}
         >
           {lang.map((l) => (
@@ -53,6 +60,7 @@ const Search = () => {
           value={secondLang}
           onChange={(e) => {
             setSecondLang(e.currentTarget.value);
+            setLastLangs([lastLangs[0], e.currentTarget.value]);
           }}
         >
           {lang.map((l) => (
@@ -61,28 +69,36 @@ const Search = () => {
             </option>
           ))}
         </select>
-        <button className="btn" onClick={handleTranslate}>Перевести</button>
+        <button className="btn" onClick={handleTranslate}>
+          Перевести
+        </button>
       </div>
-      {a ? a?.error ? (
-        a.error
-      ) : a?.data?.def && a.data.def.length > 0 ? (
-        <div>
-          <p>
-            Перевод: {a.data.def.map((x) => x.tr.map((x) => x.text)[0]) + " "}
-          </p>
-          {a.data.def[0].tr[0].syn && (
-            <p>Синонимы: {a.data.def[0].tr[0].syn?.map((x) => x.text) + " "}</p>
-          )}
-          {a.data.def[0].tr[0].mean && (
+      {a ? (
+        a?.error ? (
+          a.error
+        ) : a?.data?.def && a.data.def.length > 0 ? (
+          <div>
             <p>
-              Значение слова:
-              {a.data.def[0].tr[0].mean?.map((x) => x.text) + " "}
+              Перевод: {a.data.def.map((x) => x.tr.map((x) => x.text)[0]) + " "}
             </p>
-          )}
-        </div>
+            {a.data.def[0].tr[0].syn && (
+              <p>
+                Синонимы: {a.data.def[0].tr[0].syn?.map((x) => x.text) + " "}
+              </p>
+            )}
+            {a.data.def[0].tr[0].mean && (
+              <p>
+                Значение слова:{" "}
+                {a.data.def[0].tr[0].mean?.map((x) => x.text).join(", ")}
+              </p>
+            )}
+          </div>
+        ) : (
+          "Нет перевода"
+        )
       ) : (
-        "Нет перевода"
-      ) : ""}
+        ""
+      )}
     </div>
   );
 };
